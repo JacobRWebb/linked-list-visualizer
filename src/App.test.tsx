713cbecce3437902e3429d5f995ff9bf75d10e54
JacobRWebb <xxpwnage37xx@gmail.com>
2022-04-01 20:48:26 -0500

    Random Test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import ReactDOM from "react-dom/client";
 
-test('renders learn react link', () => {
-  render(<App />);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+test("renders learn react link", () => {
+  root.render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
